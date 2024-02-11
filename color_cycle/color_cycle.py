ad6182cbfca6d6b39c740f2bf3854df13493dd4e
TotoroTron <brian.cheng.1997@gmail.com>
2024-02-10 21:19:45 -0500

    need to figure out how to request a clock frequency in the constraints file. otherwise need a clock divider or enable pulse generator to drive fsm

--- color_cycle/color_cycle.py
+++ color_cycle/color_cycle.py
@@ -16,26 +16,22 @@ def rgb_layout(r_bits, g_bits, b_bits):
 class ControlUnit(Elaboratable):
     def __init__(self, width=8):
         self.width      = width
-        self.r_led   = Signal()
-        self.g_led   = Signal()
-        self.b_led   = Signal()
     
     def elaborate(self, platform):
         m = Module()
 
-        max        = Const(2**self.width - 1)
-        rgb_led    = Signal(rgb_layout(1, 1, 1))
-        rgb_count  = Signal(rgb_layout(self.width, self.width, self.width))
-        duty       = Signal(self.width)
-        rgb_phase  = Signal(ColorPhase)
+        max         = Const(2**self.width - 1)
+        rgb_led     = Signal(rgb_layout(1, 1, 1))
+        rgb_count   = Signal(rgb_layout(self.width, self.width, self.width))
+        duty        = Signal(self.width)
+        rgb_phase   = Signal(ColorPhase)
+        rgb_out     = platform.request("rgb_led")
 
         m.d.comb += [
-            self.r_led.eq(rgb_led.red),
-            self.g_led.eq(rgb_led.green),
-            self.b_led.eq(rgb_led.blue),
+            rgb_out.r.eq(rgb_led.red),
+            rgb_out.g.eq(rgb_led.green),
+            rgb_out.b.eq(rgb_led.blue),
         ]
-
-        # rgb_led = platform.request("rgb_led", "r")
 
         with m.If(duty < rgb_count.red):
             m.d.sync += rgb_led.red.eq(1)
@@ -101,38 +97,35 @@ class ControlUnit(Elaboratable):
 
         return m
 
-# --- TEST ---
-from amaranth.sim import Simulator
+
+# --- BUILD ---
+from amaranth_boards.arty_z7 import ArtyZ720Platform
+platform = ArtyZ720Platform()
+platform.toolchain="Xray"
+platform.build(ControlUnit(), do_program=True)
 
 
-dut = ControlUnit(width=8)
-def bench():
-    # Observe two full color cycles
-    for _ in range(256*256*3*2):
-        yield
-
-sim = Simulator(dut)
-sim.add_clock(1e-6) # 1 MHz
-sim.add_sync_process(bench)
-with sim.write_vcd("color_cycle.vcd"):
-    sim.run()
-# --- CONVERT ---
-from amaranth.back import verilog
-
-
-top = ControlUnit(width=8)
-with open("color_cycle.v", "w") as f:
-    f.write(verilog.convert(top, ports=[dut.r_led, dut.g_led, dut.b_led]))
+# # --- TEST ---
+# from amaranth.sim import Simulator
+# dut = ControlUnit(width=8)
+# def bench():
+#     # Observe two full color cycles
+#     for _ in range(256*256*3*2):
+#         yield
+# sim = Simulator(dut)
+# sim.add_clock(1e-6) # 1 MHz
+# sim.add_sync_process(bench)
+# with sim.write_vcd("color_cycle.vcd"):
+#     sim.run()
 
 
+# # --- CONVERT ---
+# from amaranth.back import verilog
+# top = ControlUnit(width=8)
+# with open("color_cycle.v", "w") as f:
+#     f.write(verilog.convert(top, ports=[dut.r_led, dut.g_led, dut.b_led]))
 
 
-# from amaranth_boards.arty_z7 import ArtyZ720Platform
-# # ArtyZ720Platform().build(LEDBlinker(), do_program=True)
 
-# # Using PRJXRAY instead of default Vivado
-# # Add AMARANTH_ENV_XRAY to environment PATH variables and set to prjxray directory
-# # Also add PYTHONPATH and set to same prjxray directory
-# platform = ArtyZ720Platform()
-# platform.toolchain="Xray"
-# platform.build(ColorCycleAHDL(), do_program=True)
+
+
